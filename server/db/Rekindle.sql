15a8316c7c623f0a30f344c24711f9c3183248fa
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-14 17:02:09 -0400

    add users to seed file

--- server/db/Rekindle.sql
+++ server/db/Rekindle.sql
@@ -50,4 +50,19 @@ CREATE TABLE hangoutparticipants (
 /* password for test is testtest */
 INSERT INTO users (username, password, email, full_name, user_img)
   VALUES ('Tom', '$2a$10$tHvwaxuv8qwR18wMXmQesuUWOxFT3xjQTTnvauZwYrZ6bM/r53kVW',
-  'test@gmail.com', 'Tester Testy', 'https://openclipart.org/download/247324/abstract-user-flat-1.svg');
+  'test@gmail.com', 'Tom Waters', 'https://openclipart.org/download/247324/abstract-user-flat-1.svg'), 
+  ('Mary', '$2a$10$tHvwaxuv8qwR18wMXmQesuUWOxFT3xjQTTnvauZwYrZ6bM/r53kVW',
+  'test@gmail.com', 'Mary Brooke', 'https://openclipart.org/download/247324/abstract-user-flat-1.svg'), 
+  ('June', '$2a$10$tHvwaxuv8qwR18wMXmQesuUWOxFT3xjQTTnvauZwYrZ6bM/r53kVW',
+  'test@gmail.com', 'June Davis', 'https://openclipart.org/download/247324/abstract-user-flat-1.svg');
+
+INSERT INTO friendships (friend_initial, friend_befriended, befriended_user_status, lastHangoutDate)
+VALUES (1, 2, 'accepted', NULL), (1, 3, 'accepted', null)
+
+CREATE TABLE friendships ( 
+  friendship_id SERIAL PRIMARY KEY, 
+  friend_initial INTEGER REFERENCES users, 
+  friend_befriended INTEGER REFERENCES users, 
+  befriended_user_status VARCHAR, 
+  lastHangoutDate date
+);
