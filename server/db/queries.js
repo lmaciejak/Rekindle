80431392e877f97c0fe011641b36eacdfa98c28f
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-15 14:00:09 -0400

    add search by user query

--- server/db/queries.js
+++ server/db/queries.js
@@ -3,25 +3,30 @@ const authHelpers = require("../auth/helpers");
 const passport = require("../auth/local");
 
 function createUser(req, res, next) {
-  console.log('createuser')
-  if(req.body.password.length <= 6) { 
-    res.status(200).json({ 
+  console.log("createuser");
+  if (req.body.password.length <= 6) {
+    res.status(200).json({
       message: `password must be longer than 6 characters`
-    })
-    return
+    });
+    return;
   }
   const hash = authHelpers.createHash(req.body.password);
-  console.log('hash', hash)
-  console.log('req.body.username',req.body.username)
+  console.log("hash", hash);
+  console.log("req.body.username", req.body.username);
   db
     .none(
       "INSERT INTO users (username, password, email, full_name ) VALUES (${username}, ${password}, ${email}, ${full_name} )",
-      { username: req.body.username, password: hash, email: req.body.email, full_name: req.body.full_name }
+      {
+        username: req.body.username,
+        password: hash,
+        email: req.body.email,
+        full_name: req.body.full_name
+      }
     )
     .then(() => {
       res.status(200).json({
         message: `created user: ${req.body.username}`
-      })
+      });
     })
     .catch(err => {
       console.log(err);
@@ -48,21 +53,37 @@ function loginUser(req, res, next) {
   })(req, res, next);
 }
 
-function getUserFriends(req, res, next) { 
-  db.any(
-    `SELECT * FROM friendships JOIN users ON(friend_befriended=user_id) WHERE friend_initial=$1 OR friend_befriended=$1`, 
-    [req.params.userID]
-  )
-  .then(data => {
-    res.json(data);
-  })
-  .catch(error => {
-    res.json(error);
-  });
+function getUserFriends(req, res, next) {
+  db
+    .any(
+      `SELECT * FROM friendships JOIN users ON(friend_befriended=user_id) WHERE friend_initial=$1 OR friend_befriended=$1`,
+      [req.params.userID]
+    )
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+}
+
+function searchByUser(req, res, next) {
+  db
+    .any(
+      `SELECT FULL_name, USER_id FROM users WHERE LOWER(username) LIKE LOWER('%${
+        req.params.search
+      }%') OR LOWER(full_name) LIKE LOWER('%${req.params.search}%')`
+    )
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
 }
 
 module.exports = {
-createUser,
-loginUser, 
-getUserFriends,
-}
+  createUser,
+  loginUser,
+  getUserFriends
+};
