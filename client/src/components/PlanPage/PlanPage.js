d244a5c2d423a8e25ebc4dd87cc131f29a0ab0d1
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-22 23:30:45 -0400

    display hangout info on plan page

--- client/src/components/PlanPage/PlanPage.js
+++ client/src/components/PlanPage/PlanPage.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Route, Switch } from "react-router";
 import { Redirect } from "react-router";
 import NavBar from "../NavBar/NavBar";
+import "./PlanPage.css";
 
 class PlanPage extends React.Component {
   constructor() {
@@ -19,7 +20,7 @@ class PlanPage extends React.Component {
       .get(`/users/gethangoutinfo/2`)
       .then(res => {
         this.setState({
-          hangoutInfo: res.data
+          hangoutInfo: res.data[0]
         });
       })
       .catch(err => {
@@ -30,10 +31,20 @@ class PlanPage extends React.Component {
   };
 
   render() {
+    console.log("hangoutinfo", this.state.hangoutInfo);
+    console.log(
+      "date",
+      new Date(this.state.hangoutInfo.availability_starttime)
+    );
+    const dateStart = new Date(this.state.hangoutInfo.availability_starttime);
     const { user } = this.state;
     return (
       <div className="planPageContainer">
         <NavBar />
+        <div className="planPageContent">
+          <h1> {dateStart.toString()} </h1>
+          <h1> hello </h1>
+        </div>
       </div>
     );
   }
