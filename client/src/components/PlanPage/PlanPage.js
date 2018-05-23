b06f4917145443ff9d8b9d46aac7923c9eaf1735
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-22 22:59:27 -0400

    add axios call to plan

--- client/src/components/PlanPage/PlanPage.js
+++ client/src/components/PlanPage/PlanPage.js
@@ -4,28 +4,36 @@ import { Route, Switch } from "react-router";
 import { Redirect } from "react-router";
 import NavBar from "../NavBar/NavBar";
 
-
-
 class PlanPage extends React.Component {
   constructor() {
     super();
     this.state = {
       user: "",
-      fetchingUser: true
+      fetchingUser: true,
+      hangoutInfo: []
     };
   }
 
   componentDidMount = () => {
-
-  }
+    axios
+      .get(`/users/gethangoutinfo/2`)
+      .then(res => {
+        this.setState({
+          hangoutInfo: res.data
+        });
+      })
+      .catch(err => {
+        this.setState({
+          message: `${err.response}`
+        });
+      });
+  };
 
   render() {
     const { user } = this.state;
     return (
       <div className="planPageContainer">
-      <NavBar />
-
-
+        <NavBar />
       </div>
     );
   }
