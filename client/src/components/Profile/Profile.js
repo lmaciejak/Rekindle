949404432c675e8816f9d7d1fb83dfeb7f82e86d
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-23 17:47:58 -0400

    update user profile

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -6,22 +6,45 @@ import NavBar from "../NavBar/NavBar";
 
 
 class Profile extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       user: "",
       fetchingUser: true,
-      hangoutInfo: []
+      hangoutInfo: [], 
+      profileID: this.props.profileID['profileID']
     };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(`/users/gethangoutinfo/2`)
+      .then(res => {
+        this.setState({
+          hangoutInfo: res.data[0]
+        });
+      })
+      .catch(err => {
+        this.setState({
+          message: `${err.response}`
+        });
+      });
+  };
+
+  handleAddFriend = () => { 
+    axios 
+    
   }
 
   render() {
     const { user } = this.state;
+    console.log('this.state.profileID *****', this.state.profileID)
     return (
       <div className="planPageContainer">
         <NavBar />
         <div className="planPageContent">
           <h1> User Profile Page </h1>
+          <button onClick={this.handleAddFriend}> Add Friend </button> 
         </div>
       </div>
     );
