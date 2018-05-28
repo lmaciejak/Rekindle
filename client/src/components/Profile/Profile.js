206ddfcb0d68d32c90be7205d0b6bb8753485a52
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-27 23:33:40 -0400

    update profile button

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -2,10 +2,9 @@ import React from "react";
 import axios from "axios";
 import { Route, Switch } from "react-router";
 import { Redirect } from "react-router";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
-import './Profile.css'
-
+import "./Profile.css";
 
 class Profile extends React.Component {
   constructor(props) {
@@ -13,18 +12,18 @@ class Profile extends React.Component {
     this.state = {
       user: "",
       fetchingUser: true,
-      profileInfo: [], 
-      profileID: this.props.profileID['profileID'], 
-      reload: false, 
-      message: ''
+      profileInfo: [],
+      profileID: this.props.profileID["profileID"],
+      reload: false,
+      message: ""
     };
   }
 
-  loadProfile = (props) => { 
+  loadProfile = props => {
     axios
-      .get(`/users/getprofile/${this.props.profileID['profileID']}`)
+      .get(`/users/getprofile/${this.props.profileID["profileID"]}`)
       .then(res => {
-        console.log('RES!!!!!', res)
+        console.log("RES!!!!!", res);
         this.setState({
           profileInfo: res.data[0]
         });
@@ -34,57 +33,76 @@ class Profile extends React.Component {
           message: `${err.response}`
         });
       });
-  }
+  };
 
   componentDidMount = () => {
-      this.loadProfile();
+    this.loadProfile();
   };
 
   componentWillReceiveProps(props) {
-      this.loadProfile();
+    this.loadProfile();
   }
 
-  componentDidUpdate(prevProps,prevState){
-    console.log('prevprops', prevProps)
-    if(prevProps !== this.props){
-      this.loadProfile()
-            // window.location.reload()
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevprops", prevProps);
+    if (prevProps !== this.props) {
+      this.loadProfile();
+      // window.location.reload()
     }
   }
 
-  handleAddFriend = () => { 
-    axios 
-    .post(`/users/sendFriendRequest`, {
-      friend_requested: this.state.profileID
-    })
-    .then(res => {
-      this.setState({
-        message: res
+  handleAddFriend = () => {
+    axios
+      .post(`/users/sendFriendRequest`, {
+        friend_requested: this.state.profileID
+      })
+      .then(res => {
+        this.setState({
+          message: res
+        });
+      })
+      .catch(err => {
+        this.setState({
+          message: `${err.response.data}`
+        });
       });
-    })
-.catch(err => {
-  this.setState({
-    message: `${err.response.data}`
-  });
-});
-  
-  }
+  };
 
   render(props) {
     const { user } = this.state;
-    console.log('state------', this.state)
-    console.log('profile propsssssss', this.props)
-    console.log('this.state.profileID *****', this.state.profileID)
+    console.log("state------", this.state);
+    console.log("profile propsssssss", this.props);
+    console.log("this.state.profileID *****", this.state.profileID);
+    console.log("first", this.props.profileID["profileID"]);
+    console.log("second", this.props.user["user_id"]);
+
     return (
       <div className="profilePageContainer">
         <NavBar />
         <div className="profilePageMain">
-        <div className="profilePageContent">
-        <img className="profileImage" src={this.state.profileInfo.user_img} />
-          <h1> {this.state.profileInfo.full_name} </h1>
-          <h4 className='userLocation'> {this.state.profileInfo.user_location} </h4>
-          <button className='addFriendButton' onClick={this.handleAddFriend}> Add Friend </button> 
-        </div>
+          <div className="profilePageContent">
+            <img
+              className="profileImage"
+              src={this.state.profileInfo.user_img}
+            />
+            <h1> {this.state.profileInfo.full_name} </h1>
+            <h4 className="userLocation">
+              {" "}
+              {this.state.profileInfo.user_location}{" "}
+            </h4>
+            {this.props.profileID.profileID ===
+            this.props.user.user_id ? (
+              <button className="addFriendButton"> Edit Profile </button>
+            ) : (
+              <button
+                className="addFriendButton"
+                onClick={this.handleAddFriend}
+              >
+                {" "}
+                Add Friend{" "}
+              </button>
+            )}
+          </div>
         </div>
       </div>
     );
