85a2ad44b1433fc10385dd36ffa9f06ad95ebe48
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-28 16:37:59 -0400

    add get request to dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,31 +1,67 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Route, Switch } from "react-router"
+import { Route, Switch } from "react-router";
 import NavBar from "../NavBar/NavBar";
-import './Dashboard.css'
+import "./Dashboard.css";
 
 class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
       user: "",
-      fetchingUser: true
+      fetchingUser: true,
+      dashboardInfo: ""
     };
   }
 
+  componentDidMount = () => {
+    axios
+      .get(`/users/getdashboardhangouts`)
+      .then(res => {
+        console.log("RES!!!!!", res);
+        let tracker = {};
+        let newArr = [];
+        let arr = res.data
+        arr.forEach(elem => {
+          console.log("elem", elem["usertosharewith_id"]);
+          if (!tracker[elem["usertosharewith_id"]]) {
+            tracker[elem["usertosharewith_id"]] = 1;
+            newArr.unshift(elem);
+          }
+        });
+        this.setState({
+          dashboardInfo: newArr
+        });
+      })
+      .catch(err => {
+        this.setState({
+          message: `${err.response}`
+        });
+      });
+  };
+
   render() {
     const { user } = this.state;
+    console.log("dashboard", this.state.dashboardInfo);
     return (
-      <div >
-      <NavBar />
-      <div className='hangoutDashboardContainer'> 
-      <div className="hangoutDashboardContent">
-<h2 className='dashboardTitle'> Hangout Dashboard </h2>
-</div>
-</div>
-</div>
-);
-}
+      <div className="hangoutPage">
+        <NavBar />
+        <div className="hangoutDashboardContainer">
+          <div className="hangoutDashboardContent">
+            <h2 className="dashboardTitle"> Hangout Dashboard </h2>
+            {this.state.dashboardInfo ? this.state.dashboardInfo.map((elem) => ( 
+              <div> {elem.full_name} 
+              <br /> 
+              <img src={elem.user_img} />
+              <br /> 
+              {elem.availability_starttime}
+              </div> 
+            )) : ''}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
