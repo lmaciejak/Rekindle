e1fbad87e88844a1810125d894f35368ad279afc
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-31 10:23:56 -0400

    updated for heroku

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 var pgp = require("pg-promise")({});
-var connectionString = process.env.DATABASE_URL;
-// var connectionString = "postgres://localhost/rekindle";
+// var connectionString = process.env.DATABASE_URL;
+var connectionString = "postgres://localhost/rekindle";
 var db = pgp(connectionString);
 
 module.exports = db;
